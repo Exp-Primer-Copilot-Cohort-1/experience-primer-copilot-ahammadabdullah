780a9938d94d05a54121b16a7e5f70d9fa6b72f6
Ahammad Abdullah <ahammadabdullahnew@gmail.com>
2024-01-09 17:25:50 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,62 +1,15 @@
 // create web server
+// 1. create http server
+// 2. create web server
+// 3. create route
+// 4. handle request
+// 5. start server
+// 6. listen for request
+// 7. parse request body
+// 8. create comment
+// 9. save comment to database
+// 10. send response
+// 11. send response with comment
 
-const express = require("express");
-const app = express();
-const port = 3000;
-const path = require("path");
-
-// add static files
-app.use(express.static("public"));
-
-// add body parser
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// add mongoose
-const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/comments", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-// add comment model
-const commentSchema = new mongoose.Schema({
-  name: String,
-  comment: String,
-});
-
-const Comment = mongoose.model("Comment", commentSchema);
-
-// set up routes
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
-app.post("/add-comment", (req, res) => {
-  const comment = new Comment({
-    name: req.body.name,
-    comment: req.body.comment,
-  });
-
-  comment
-    .save()
-    .then((result) => {
-      console.log(result);
-      res.redirect("/");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.get("/comments", (req, res) => {
-  Comment.find()
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// 1. create http server
+const http = require("http");
