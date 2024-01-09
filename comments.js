c61e9c9107c843a25e39ef4c8db77a125fee7032
Ahammad Abdullah <ahammadabdullahnew@gmail.com>
2024-01-09 17:31:23 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,15 +1,48 @@
-// create web server
-// 1. create http server
-// 2. create web server
-// 3. create route
-// 4. handle request
-// 5. start server
-// 6. listen for request
-// 7. parse request body
-// 8. create comment
-// 9. save comment to database
-// 10. send response
-// 11. send response with comment
+//create web server
+var express = require("express");
+var router = express.Router();
+var mongoose = require("mongoose");
+var Comments = require("../models/Comments.js");
 
-// 1. create http server
-const http = require("http");
+// GET /comments
+router.get("/", function (req, res, next) {
+  Comments.find(function (err, comments) {
+    if (err) return next(err);
+    res.json(comments);
+  });
+});
+
+// POST /comments
+router.post("/", function (req, res, next) {
+  Comments.create(req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+// GET /comments/:id
+router.get("/:id", function (req, res, next) {
+  Comments.findById(req.params.id, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+// PUT /comments/:id
+router.put("/:id", function (req, res, next) {
+  Comments.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+// DELETE /comments/:id
+router.delete("/:id", function (req, res, next) {
+  Comments.findByIdAndRemove(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+//export router to use in app.js
+module.exports = router;
